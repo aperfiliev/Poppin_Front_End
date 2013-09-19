20bf9991a1bc3c296def8937c8573f5b01f41e4e
ashykalov <ashykalov@5bad8475-d561-42e4-980f-84f2a8165adb>
2013-09-19 08:13:49 +0000

    poppinresources

---
+++ SocialMediaLogin/js/poppinservresources.js
@@ -0,0 +1,17 @@
+var poppinservres = {
+	url:{
+	gigyacontrol:"https://checkout.sandbox.netsuite.com/c.3363929/SSP%20Applications/Poppin/SocialMediaLogin/gigya.html",
+	loginservice:"https://checkout.sandbox.netsuite.com/c.3363929/Poppin/SocialMediaLogin/LoginService.ss",
+	loginpage:"https://checkout.sandbox.netsuite.com/c.3363929/Poppin/SocialMediaLogin/Login.ssp",
+	poppinglobal:"http://sandbox.poppin.com",
+	checkexistinguser:"https://forms.sandbox.netsuite.com/app/site/hosting/scriptlet.nl?script=272&deploy=1&compid=3363929&h=998696f5b08fd08e8297",
+	
+	},
+	text : {
+	forgotpassword:"1234",
+	forgotpasswordsent:"Ok. We've sent you instructions on how to update your password, aka Your Personal Key to Work Happiness. Please check your email.",
+	forgotpasswordunrecognized:'The email that you entered is not recognized, please try again or <a href="'+ '/Login.ssp' +'" style="text-decoration: underline;">create a new account</a>',
+	
+	
+	}
+};
