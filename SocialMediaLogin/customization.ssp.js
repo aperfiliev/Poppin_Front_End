2fb277cef7f5b17ad68ad01a68a464522e594981
sforostiuk <sforostiuk@5bad8475-d561-42e4-980f-84f2a8165adb>
2013-10-31 14:20:04 +0000

    ppp-262 powertip

--- SocialMediaLogin/customization.ssp.js
+++ SocialMediaLogin/customization.ssp.js
@@ -1,1 +1,17 @@
 $("td.text:last").html("<div class='phoneDiv'>866 &#8226; 926 &#8226; 4922</div>");
+
+var popuphtml = '<p class="popup_header">Ready to make your mark?</p>'
+	+'<p class="popup_text">Give us a call to get started 866.926.4922</p>';
+
+$(".customization_item").mouseenter(function(){
+	var item_id = $(this).children(":first").attr('id');
+	this.popupTimeout = setTimeout(function() {
+		powerTip.create(item_id, popuphtml, 'popup'+item_id, -262, 281);
+	}, 3000);
+});
+
+$(".customization_item").mouseleave(function(){
+	var item_id = $(this).children(":first").attr('id');
+	clearTimeout(this.popupTimeout);
+	powerTip.hide('popup'+item_id);
+});
