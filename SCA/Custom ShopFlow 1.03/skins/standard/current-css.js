d73895cbcd1330500c08a331bc8830e50c42e261
ashykalov <ashykalov@5bad8475-d561-42e4-980f-84f2a8165adb>
2014-06-03 07:37:43 +0000

    74 ticket Category layout change

--- SCA/Custom ShopFlow 1.03/skins/standard/current-css.js
+++ SCA/Custom ShopFlow 1.03/skins/standard/current-css.js
@@ -1,1 +1,1 @@
-BDQ.currentTemplatesUrl = '/Web Site Hosting Files/Live Hosting Files/ShopFlow/skins/standard/Styles-0145ccb19ffc.css';
+BDQ.currentTemplatesUrl = '/Web Site Hosting Files/Live Hosting Files/shopflow-1-03-0/skins/standard/Styles-01465c9e2636.css';
