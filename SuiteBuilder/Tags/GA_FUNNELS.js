3512177441dfa36af99dca37a5e402fa9d75ef32
rchernyshenko <rchernyshenko@5bad8475-d561-42e4-980f-84f2a8165adb>
2014-07-30 09:10:51 +0000

    PPT-133 Checkout Optimizations

--- SuiteBuilder/Tags/GA_FUNNELS.js
+++ SuiteBuilder/Tags/GA_FUNNELS.js
@@ -1,39 +1,9 @@
 <script type="text/javascript">
-if(jQuery('form#newcust').length){       
-	_gaq.push(['_trackPageview','/checkout-register']);   
-}else{
-	if(jQuery('form#login').length){
-		_gaq.push(['_trackPageview','/checkout-login']);
-	}else{
-		var mainForm = jQuery('form#main_form')
-		if(mainForm.length){
-			if(jQuery('input#category',mainForm).val() == 'billing'){                     
-				 _gaq.push(['_trackPageview','/checkout-address']);                                          
-			}
-		}else{
-			var checkoutForm = jQuery('form#checkout');
-			if(checkoutForm.length){
-				var category = jQuery('input[name=category]', checkoutForm).val();
-				switch (category) {
-					case 'paymeth':
-						_gaq.push(['_trackPageview','/checkout-paymeth']);
-						break;   
-					case 'shipping':
-						_gaq.push(['_trackPageview','/checkout-shipping']);
-						break;   
-					case 'confirm':
-						_gaq.push(['_trackPageview','/checkout-confirm']);
-						break;   
-					case 'thanks':
-						_gaq.push(['_trackPageview','/checkout-thanks']);   
-
-
-						break;
-				}
-			}else{
-				_gaq.push(['_trackPageview']);
-			}
-		}
-	}   
-}   
+if (jQuery('form#loginform').length || jQuery('form#new-customer-register').length) {
+    _gaq.push(['_trackPageview','/login-register']);
+} else if (jQuery('table#carttable').length) {
+    _gaq.push(['_trackPageview','/shopping-cart']);
+} else {
+    _gaq.push(['_trackPageview']);
+}
 </script>
