21aa90aab92b313efc92a8c4016ee93384dacf23
rchernyshenko <rchernyshenko@5bad8475-d561-42e4-980f-84f2a8165adb>
2014-11-17 08:59:46 +0000

    PPT-245 Script performance improvements

--- SuiteScripts/sc_pp_getavailablecolorsthumbnails.js
+++ SuiteScripts/sc_pp_getavailablecolorsthumbnails.js
@@ -1,30 +1,32 @@
 function getAvailableColors(request, response) {
-	try {
-		var _ids = request.getParameter("_ids").split(",");
-nlapiLogExecution('DEBUG',JSON.stringify(_ids));
-		var arrFilters = [
-				new nlobjSearchFilter('internalid', null, 'is', _ids),
-				new nlobjSearchFilter('isinactive', null, 'is', "F"),
-				new nlobjSearchFilter('isonline', null, 'is', "T"),
-				new nlobjSearchFilter('thumbnailurl', null, 'isnotempty', "T") ];
-		var column = new nlobjSearchColumn("thumbnailurl");
-		var column2 = new nlobjSearchColumn("custitem_availablecolor_thumb");
-		var arrResults = nlapiSearchRecord("item", null, arrFilters, [ column,
-				column2 ]);
-		var objResultsMap = {};
-nlapiLogExecution('DEBUG','arrResults',JSON.stringify(arrResults));
-		if (arrResults && arrResults.length) {
-			for ( var i = 0, len = arrResults.length; i < len; i++) {
-				var fileID = arrResults[i].getValue(column2), strURL = (fileID ? nlapiLoadFile(
-						fileID).getURL()
-						: arrResults[i].getValue(column)).replace("sandbox",
-						"cdn-sb");
-				objResultsMap[arrResults[i].id] = strURL;
-			}
-		}
-		response.write(JSON.stringify(objResultsMap));
-	} catch (e) {
-		getErrorMessage(e, "getAvailableColors");
-		response.write("{\"error\":1}");
-	}
+    try {
+        var _ids = request.getParameter("_ids").split(","),
+        arrFilters = [
+                new nlobjSearchFilter('internalid', null, 'is', _ids),
+                new nlobjSearchFilter('isinactive', null, 'is', "F"),
+                new nlobjSearchFilter('isonline', null, 'is', "T"),
+                new nlobjSearchFilter('thumbnailurl', null, 'isnotempty', "T") ],
+        column = new nlobjSearchColumn("thumbnailurl"),
+        column2 = new nlobjSearchColumn("custitem_availablecolor_thumb"),
+        arrResults = nlapiSearchRecord("item", null, arrFilters, [ column,
+                column2 ]),
+        objResultsMap = {},
+        len = arrResults.length,
+        searchElement,
+        i;
+        if (arrResults && len) {
+            for (i = 0; i < len; i++) {
+                searchElement = arrResults[i];
+                fileID = searchElement.getValue(column2), strURL = (fileID ? nlapiLoadFile(
+                        fileID).getURL()
+                        : searchElement.getValue(column)).replace("sandbox",
+                        "cdn-sb");
+                objResultsMap[searchElement.id] = strURL;
+            }
+        }
+        response.write(JSON.stringify(objResultsMap));
+    } catch (e) {
+        getErrorMessage(e, "getAvailableColors");
+        response.write("{\"error\":1}");
+    }
 }
--- SuiteScripts/sc_pp_menu.js
+++ SuiteScripts/sc_pp_menu.js
@@ -1,76 +1,84 @@
-var len = 0;
-
 function getMenu(request, response) {
-	var arrCols = [ new nlobjSearchColumn("name"),
-			new nlobjSearchColumn("custrecord_cateogry_url"),
-			new nlobjSearchColumn("custrecord_subcategories"),
-			new nlobjSearchColumn("custrecord_is_root"),
-			new nlobjSearchColumn("custrecord_order").setSort(),
-			(new nlobjSearchColumn("created")).setSort() ];
-	var arrMenuItems = nlapiSearchRecord("customrecord_menu_item", null, null,
-			arrCols);
-	var arrMenu = [];
-	len = arrMenuItems.length;
-	for ( var i = 0; i < arrMenuItems.length; i++)
-		if (arrMenuItems[i].getValue("custrecord_is_root") == "T") {
-			var item = fetchItem(arrMenuItems[i]);
-			arrMenu.push(item);
-		}
-	for ( var i = 0; i < arrMenu.length; i++)
-		buildMenu(arrMenuItems, arrMenu[i].c);
+    var arrCols = [ new nlobjSearchColumn("name"),
+            new nlobjSearchColumn("custrecord_cateogry_url"),
+            new nlobjSearchColumn("custrecord_subcategories"),
+            new nlobjSearchColumn("custrecord_is_root"),
+            new nlobjSearchColumn("custrecord_order").setSort(),
+            (new nlobjSearchColumn("created")).setSort() ],
+        arrMenuItems = nlapiSearchRecord("customrecord_menu_item", null, null, arrCols),
+        arrMenu      = [],
+        len          = arrMenuItems.length,
+        arrMenuItem,
+        i;
+    for (i = 0; i < len; i++) {
+        arrMenuItem = arrMenuItems[i];
+        if (arrMenuItem.getValue("custrecord_is_root") == "T") {
+            var item = fetchItem(arrMenuItem);
+            arrMenu.push(item);
+        }
+    }
+    
+    for (i = 0; i < arrMenu.length; i++)
+        buildMenu(arrMenuItems, arrMenu[i].c);
 
-	response.write(JSON.stringify(arrMenu));
+    response.write(JSON.stringify(arrMenu));
 }
 
 function getItem(intId, arrMenuItems) {
-	var i = 0;
-	var len = arrMenuItems.length;
-	while (i < len && arrMenuItems[i].id != intId)
-		i++;
-	len--;
-	return fetchItem(splice(arrMenuItems, i));
+    var i   = 0,
+        len = arrMenuItems.length;
+    while (i < len && arrMenuItems[i].id != intId)
+        i++;
+    len--;
+    return fetchItem(splice(arrMenuItems, i));
 }
 
 function fetchItem(objSearchResult) {
-	var children = objSearchResult.getValue("custrecord_subcategories").split(
-			",");
-	var arrChildren = [];
-	for ( var i = 0, len = children.length; i < len; i++)
-		if (children[i].replace(/\s+/g, "").length)
-			arrChildren.push(children[i]);
-	var item = {
-		"i" : objSearchResult.getValue("custrecord_cateogry_url"),
-		"n" : objSearchResult.getValue("name").replace(/\s*::.*/g,""),
-		"o" : objSearchResult.getValue("custrecord_order"),
-	};
-	if (arrChildren.length)
-		item.c = arrChildren;
-	return item;
+    var children    = objSearchResult.getValue("custrecord_subcategories").split(","),
+        arrChildren = [];
+    for ( var i = 0, len = children.length; i < len; i++)
+        if (children[i].replace(/\s+/g, "").length)
+            arrChildren.push(children[i]);
+    var item = {
+        "i" : objSearchResult.getValue("custrecord_cateogry_url"),
+        "n" : objSearchResult.getValue("name").replace(/\s*::.*/g,""),
+        "o" : objSearchResult.getValue("custrecord_order"),
+    };
+    if (arrChildren.length)
+        item.c = arrChildren;
+    return item;
 }
 
 function buildMenu(arrMenuItems, arrMenu) {
-	if (arrMenu) {
-		for ( var i = 0, len = arrMenu.length; i < len; i++) {
-			var item = getItem(arrMenu[i], arrMenuItems);
-			buildMenu(arrMenuItems, item.c);
-			arrMenu[i] = item;
-		}
-		arrMenu.sort(function(a, b){ if (a.o < b.o) return -1; if (a.o > b.o) return 1; return 0; });
-	}
+    if (arrMenu) {
+        for ( var i = 0, len = arrMenu.length; i < len; i++) {
+            var item = getItem(arrMenu[i], arrMenuItems);
+            buildMenu(arrMenuItems, item.c);
+            arrMenu[i] = item;
+        }
+        arrMenu.sort(function(a, b) {
+            if (a.o < b.o)
+                return -1;
+            else if (a.o > b.o)
+                return 1;
+            else
+                return 0;
+        });
+    }
 }
 
 // Array.pop doesn't remove the desired item as espected
 function pop(arr, arrDest) {
-	return splice(arr, 0);
+    return splice(arr, 0);
 }
 
 // Array.splice doesn't remove the desired item as espected
 function splice(arr, i) {
-	if (!(arr && arr.length))
-		return;
-	var item = arr[i];
-	for ( var k = i + 1, len = arr.length; k < len; k++)
-		arr[k - 1] = arr[k];
-	// arr.length--; doesn't work
-	return item;
+    if (!(arr && arr.length))
+        return;
+    var item = arr[i];
+    for ( var k = i + 1, len = arr.length; k < len; k++)
+        arr[k - 1] = arr[k];
+    // arr.length--; doesn't work
+    return item;
 }
